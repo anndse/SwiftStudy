df3b935d6fc5f07a8fc44d7ad1e13e487d5332d4
wanghui <523693730@qq.com>
2015-03-28 16:49:07 +0800

    call init to create a new instance of fahrenheit

--- InitTest/InitTest/main.swift
+++ InitTest/InitTest/main.swift
@@ -10,3 +10,14 @@ import Foundation
 
 println("Hello, World!")
 
+struct Fahrenheit {
+    var temperature: Double
+    
+    init(){
+        temperature = 32.0
+    }
+}
+
+var f = Fahrenheit()
+    
+println("The default temperature is \(f.temperature)° fahrenheit")
